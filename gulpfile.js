2e570259c0c5e86843354b3777b55df05f8363f0
Anastasiia Skrypal <a.skrypal1169@gmail.com>
2021-03-11 08:47:43 +0200

    pixel perfect

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require("gulp"),
 	scss = require("gulp-sass"),
 	autoprefixer = require("gulp-autoprefixer"),
-	browserSync = require("browser-sync");
+	iconfont = require('gulp-iconfont'),
+	browserSync = require("browser-sync"),
+	iconfontCss = require('gulp-iconfont-css');
 
 gulp.task("sass", function() {
 	return gulp.src("scss/**/*.scss")
@@ -21,7 +23,28 @@ gulp.task("reload", function(done) {
 	done()
 })
 
+gulp.task('iconfont', function(){
+	var runTimestamp = Math.round(Date.now()/1000);
+
+	return gulp.src(['./images/icons/*.svg'])
+		.pipe(iconfontCss({
+			fontName: 'iconfont',
+			path: './scss/global/_icons_template.scss',
+			targetPath: '../scss/global/_icons.scss',
+			fontPath: '../fonts/',
+			firstGlyph: 0xf120 
+		})).pipe(iconfont({
+			fontName: 'iconfont',
+			formats: ['svg', 'ttf', 'eot', 'woff', 'woff2'],
+			normalize: true,
+			prependUnicode: true,
+			fontHeight: 1001,
+			timestamp: runTimestamp
+		})).pipe(gulp.dest('./fonts/'));
+});
+
 gulp.task("watch", function() {
 	gulp.watch("scss/**/*.scss", gulp.series("sass", "reload"))
 	gulp.watch("./**/*.html", gulp.series("reload"))
+	gulp.watch('./img/icons/*.svg', gulp.series('iconfont'))
 })
