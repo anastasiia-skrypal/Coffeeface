eec28a18453ba0943740d8dc967bb51339c31d6d
Anastasiia Skrypal <a.skrypal1169@gmail.com>
2021-03-26 17:18:06 +0200

    no message

---
+++ Everad/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+  	items: 1,
+    center: true,
+    rtl: true,
+    loop: true,
+    nav: true,
+    dots: true,
+    // responsive:{
+    //   0:{
+    //       items:2.5,
+    //       nav:false
+    //   },
+    //   785:{
+    //       items:5,
+    //       nav:false
+    //   },
+    // },
+  });
+}); 
